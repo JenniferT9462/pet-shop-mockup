8e928a1df7b9cbab58f5c90228eec5869c8b367e
Jennifer L Tarleton <63979357+JenniferT9462@users.noreply.github.com>
2024-10-22 16:12:14 -0500

    Add PetHeroImage story component

---
+++ src/components/PetHeroImage.stories.jsx
@@ -0,0 +1,13 @@
+import PetHeroImage from './PetHeroImage';
+
+const meta = {
+  component: PetHeroImage,
+};
+
+export default meta;
+
+export const Default = {
+  args: {
+    src: "public/img/hero-dog.png"
+  }
+};
