dc5e252ba16fbcad136d41c04455cc1147e31210
Jennifer L Tarleton <63979357+JenniferT9462@users.noreply.github.com>
2024-10-22 21:25:29 -0500

    Styled CTA

--- src/components/PetCallToAction.js
+++ src/components/PetCallToAction.js
@@ -2,8 +2,8 @@ import PropTypes from "prop-types";
 
 export default function PetCallToAction(props) {
   return (
-    <div>
-      TODO: Finish this component based on the design in public/mockups/CTA.png
+    <div className="rounded-full border-8 border-rose-600 w-100">
+      <button className="bg-white text-blue-900 font-bold py-2 px-4 rounded-full w-full">Access Now</button>
     </div>
   );
 }
