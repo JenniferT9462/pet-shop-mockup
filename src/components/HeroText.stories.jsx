41a2c7cc39ab41b40b64d4416fd87e578363c8d6
Jennifer L Tarleton <63979357+JenniferT9462@users.noreply.github.com>
2024-10-22 23:45:36 -0500

    Add story for HeroText and deleted a story that was not needed

---
+++ src/components/HeroText.stories.jsx
@@ -0,0 +1,27 @@
+import HeroText from './HeroText';
+
+const meta = {
+  component: HeroText,
+};
+
+export default meta;
+
+export const Default = {
+  args:{
+    heroText:"Bring happiness to your home.\nFind the ideal pet for your lifestyle!"
+  }
+};
+
+export const English = {
+  args: {
+    heroText: "Bring happiness to your home.\nFind the ideal pet for your lifestyle"
+  }
+};
+
+
+
+export const Portuguese = {
+  args: {
+    heroText: "Leve\na felicidade\npara o seu lar\n \nEncontre o animal de estimação ideal\npara seu estilo de vida!"
+  }
+};
