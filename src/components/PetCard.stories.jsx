b7d04b511719f4c9638d3951740df5682d1ba96d
Jennifer L Tarleton <63979357+JenniferT9462@users.noreply.github.com>
2024-10-24 15:10:11 -0500

    PetCard stories

---
+++ src/components/PetCard.stories.jsx
@@ -0,0 +1,35 @@
+import PetCard from './PetCard';
+
+const meta = {
+  component: PetCard,
+};
+
+export default meta;
+
+export const Default = {
+  args: {
+    emoji: "🐶",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer eu massa lectus. Integer ipsum dolor, laoreet eu ornare ac, dignissim vitae ex. \n"
+  }
+};
+
+export const SeeingEyeDog = {
+  args: {
+    emoji: "🦮",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer eu massa lectus. Integer ipsum dolor, laoreet eu ornare ac, dignissim vitae ex. \n"
+  }
+};
+
+export const Cat = {
+  args: {
+    emoji: "🐈",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer eu massa lectus. Integer ipsum dolor, laoreet eu ornare ac, dignissim vitae ex. \n"
+  }
+};
+
+export const McHammie = {
+  args: {
+    emoji: "🐹",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer eu massa lectus. Integer ipsum dolor, laoreet eu ornare ac, dignissim vitae ex. \n"
+  }
+};
