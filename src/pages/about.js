50f841eaac839eb8465c8b016c4d3b7bb0c306e0
Jennifer L Tarleton <63979357+JenniferT9462@users.noreply.github.com>
2024-10-24 14:43:30 -0500

    Added SecondaryImage and PetAboutText components to about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,6 +1,6 @@
-// TODO: create a new page component based on the bottom hae of the design in public/mockups/home.png
+// TODO: create a new page component based on the bottom half of the design in public/mockups/home.png
 
-// You fill first need to create three components: SecondaryImage, PetAboutText, and PetCard in the src/components folder. 
+// You will first need to create three components: SecondaryImage, PetAboutText, and PetCard in the src/components folder. 
 
 // You should also create stories for each of these components in the src/components folder using storybook.
 
@@ -10,3 +10,24 @@
 // 2. Create a component called PetAboutText based on the design in public/mockups/description-title.png
 // 3. Create a component called PetCard based on the design in public/mockups/amigo-card.png
 // Optional: include the "Header from the Home page" at the top of the About page
+import SecondaryImage from '../components/SecondaryImage';
+import PetHeaderLogo from '../components/PetHeaderLogo';
+import PetHeaderMenu from '../components/PetHeaderMenu';
+import PetAboutText from '@/components/PetAboutText';
+
+export default function AboutPage() {
+    return (
+        <div className="bg-rose-500">
+            <div class="container mx-auto px-4">
+                <div className="flex justify-around py-3">
+                    <PetHeaderLogo logoSrc="/img/paw-face-sm.png" brandName="FindAFriend" />
+                    <PetHeaderMenu menuItems={["Home", "Sobre o app", "Animais disponíveis", "Sobre a FindAFriend", "Contact"]} />
+                </div>
+            </div>
+            <main>
+                <PetAboutText />
+                <SecondaryImage src="/img/pet-app-layers.png"/>
+            </main>
+        </div>
+    );
+}
