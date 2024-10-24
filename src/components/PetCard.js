cb184b83af6b39123a4d6d2816cc42409ca0fc09
Jennifer L Tarleton <63979357+JenniferT9462@users.noreply.github.com>
2024-10-24 15:09:59 -0500

    Made PetCard component

---
+++ src/components/PetCard.js
@@ -0,0 +1,16 @@
+import PropTypes from "prop-types";
+
+export default function PetCard(props) {
+    return (
+        <div className="bg-rose-100 border-2 border-rose-400 rounded-3xl p-4">
+            <div className="text-4xl mb-7">{props.emoji}</div>
+            <div className="text-black">{props.description}</div>
+        </div>
+    );
+}
+
+
+PetCard.propTypes = {
+    emoji: PropTypes.string,
+    description: PropTypes.string.isRequired
+  };
